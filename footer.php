8b159af5a757f15df5ef64b67319477d3eb6d82e
YigithanGumus <yigithangumuss@gmail.com>
2023-02-10 00:57:31 +0300

    yigithan
    dosyaları yükleidm

---
+++ footer.php
@@ -0,0 +1,112 @@
+<footer class="footer">
+              <div class="container">
+                <div class="row info-container">
+                  <div class="col-12 col-lg-auto social-container">
+                    <a href="https://www.facebook.com/ajanskumsal" rel="noreferrer" target="_blank" class="fab fa-facebook" aria-label="facebook"></a>
+                    <a href="https://twitter.com/ajanskumsal" rel="noreferrer" target="_blank" class="fab fa-twitter" aria-label="twitter"></a>
+                    <a href="https://www.instagram.com/kumsalajans/" rel="noreferrer" target="_blank" class="fab fa-instagram" aria-label="instagram"></a>
+                    <a href="https://www.linkedin.com/company/kumsalajans/" rel="noreferrer" target="_blank" class="fab fa-linkedin" aria-label="linkedin"></a>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12 blog-title">
+                    <p class="scaled-p">Blog Başlıkları</p>
+                  </div>
+                  <div class="col-12">
+                    <div class="row blog-container">
+                      <div class="col-12 col-md-6 col-lg-4">
+                        <a href="https://kumsalajans.com/blog/web-tasarim/history-of-web-design" title="History Of Web Design">
+                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">History Of Web Design</span>
+                        </a>
+                      </div>
+                      <div class="col-12 col-md-6 col-lg-4">
+                        <a href="https://kumsalajans.com/blog/web-tasarim/what-is-web-design" title="What is web design?">
+                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">What is web design?</span>
+                        </a>
+                      </div>
+                      <div class="col-12 col-md-6 col-lg-4">
+                        <a href="https://kumsalajans.com/blog/dijital-pazarlama/sosyal-medya-ajansi-nedir" title="Sosyal Medya Ajansları">
+                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Sosyal Medya Ajansları</span>
+                        </a>
+                      </div>
+                      <div class="col-12 col-md-6 col-lg-4">
+                        <a href="https://kumsalajans.com/blog/web-tasarim/web-sitesi-yenileme-nedir" title="Web Sitesi Yenileme Nedir? | Hangi Durumlarda Yapılabilir?">
+                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Web Sitesi Yenileme Nedir? | Hangi Durumlarda Yapılabilir?</span>
+                        </a>
+                      </div>
+                      <div class="col-12 col-md-6 col-lg-4">
+                        <a href="https://kumsalajans.com/blog/dijital-pazarlama/dijital-pazarlama-ajansi-nedir" title="Dijital Pazarlama Ajansı Nedir? | Hizmetleri Nelerdir?">
+                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Dijital Pazarlama Ajansı Nedir? | Hizmetleri Nelerdir?</span>
+                        </a>
+                      </div>
+                      <div class="col-12 col-md-6 col-lg-4">
+                        <a href="https://kumsalajans.com/blog/dijital-pazarlama/instagram-reklam-danismanligi-nedir" title="Instagram Reklam Danışmanlığı Nedir? | Neleri Kapsar?">
+                         <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Instagram Reklam Danışmanlığı Nedir? | Neleri Kapsar?</span>
+                       </a>
+                     </div>
+                     <div class="col-12 col-md-6 col-lg-4">
+                      <a href="https://kumsalajans.com/blog/dijital-pazarlama/reklam-ajansi-nedir" title="Reklam Ajansı Nedir? | Neden Gereklidir?">
+                        <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Reklam Ajansı Nedir? | Neden Gereklidir?</span>
+                      </a>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                      <a href="https://kumsalajans.com/blog/dijital-pazarlama/google-reklam-danismanligi-nedir" title="Google Reklam Danışmanlığı Nedir? | Neden Önemlidir?">
+                        <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Google Reklam Danışmanlığı Nedir? | Neden Önemlidir?</span>
+                      </a>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                      <a href="https://kumsalajans.com/blog/web-ve-mobil-yazilim/nft-game-nedir" title="NFT Game Nedir? | NFT Game Gelişimi">
+                        <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">NFT Game Nedir? | NFT Game Gelişimi</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row copyright">
+                <div class="col-sm">
+                  Telif Hakkı © Uscew 2023. <span>Tüm Hakları Saklıdır.</span>
+                </div>
+                <div class="col-auto mt-3 mt-sm-0">
+                  <a href="https://kumsalajans.com/aydinlatma-metni" class="d-flex align-items-center">
+                    <i class="fad fa-chevron-double-right mr-1"></i>
+                    
+                  </a>
+                </div>
+              </div>
+            </div>
+            <a href="https://wa.me/+908503037441" class="fixed-whatsapp">
+              <img src="storage/media/1463/whatsapp.svg" alt="">
+            </a>
+          </footer>
+          <div class="fixed-buttons">
+            <div class="row no-gutters">
+              <div class="col-12 d-block d-md-none">
+                <a href="tel:+90 216 706 60 64" class="phone gads-click-conversion">
+                  <i class="fad fa-phone"></i>
+                </a>
+              </div>
+              <div class="col-12 d-none d-md-block">
+                <a href="javascript:void(0);" class="scroll-top-body up">
+                  <i class="fad fa-chevron-up"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="cookie-container d-none">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col">
+                   </div>
+                  <div class="col-auto">
+                    <a href="#" class="button button-gray-black cookie-accept">
+                      <span class="d-sm-none"><i class="fad fa-times"></i></span>
+                      <span class="d-none d-sm-flex align-items-center"><i class="fad fa-chevron-double-right mr-1"></i> Kabul Et</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+                <script type="text/javascript" src="assets/front/js/js.js"></script>
+
+              </body>
+              </html>
