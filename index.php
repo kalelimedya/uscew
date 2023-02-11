eb24ec5697893b4bd1ef930c5d3c0e2e557fc409
erenekmekci <erenekm@gmail.com>
2023-02-12 00:42:52 +0300

    blog

--- index.php
+++ index.php
@@ -1,127 +1,107 @@
-<style>
-  .carousel {
-    margin: 30px 0;
-    background: white;
-    position: relative;
-    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
-  }
 
-  .carousel:after {
-    content: "";
+<style>
+.carousel{
+	margin: 30px 0;
+    background: white;
+  	position: relative;
+    box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+}
+.carousel:after {
+	content: "";
     position: absolute;
     z-index: -1;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
+    box-shadow: 0 0 20px rgba(0,0,0,0.8);
     height: 60px;
     bottom: 0px;
     left: 10px;
     right: 10px;
     border-radius: 100px / 20px;
-  }
-
-  .carousel .item {
+}
+.carousel .item {
     text-align: center;
-    overflow: hidden;
+	overflow: hidden;
     height: 475px;
-  }
-
-  .carousel .item img {
-    max-width: 100%;
-    margin: 0 auto;
-    /* Align slide image horizontally center in Bootstrap v3 */
-  }
-
-  .carousel .carousel-control {
-    width: 50px;
+}
+.carousel .item img {
+	max-width: 100%;
+    margin: 0 auto; /* Align slide image horizontally center in Bootstrap v3 */
+}
+.carousel .carousel-control {
+	width: 50px;
     height: 50px;
     background: #000;
     margin: auto 0;
-    opacity: 0.8;
-  }
-
-  .carousel .carousel-control:hover {
-    opacity: 0.9;
-  }
-
-  .carousel .carousel-control i {
+	opacity: 0.8;
+}
+.carousel .carousel-control:hover {
+	opacity: 0.9;
+}
+.carousel .carousel-control i {
     font-size: 41px;
-  }
-
-  .carousel-caption h3,
-  .carousel-caption p {
-    color: #fff;
-    display: inline-block;
-    font-family: 'Oswald', sans-serif;
-    text-shadow: none;
-    margin-bottom: 20px;
-  }
-
-  .carousel-caption h3 {
-    background: rgba(0, 0, 0, 0.9);
-    padding: 12px 24px;
-    font-size: 40px;
-    text-transform: uppercase;
-  }
-
-  .carousel-caption p {
-    background: black;
-    padding: 10px 20px;
-    font-size: 20px;
-    font-weight: 300;
-  }
-
-  .carousel-action {
-    padding: 10px 0 30px;
-  }
-
-  .carousel-action .btn {
-    min-height: 34px;
-    border-radius: 3px;
-    margin: 3px;
-    min-width: 150px;
-    text-transform: uppercase;
-    font-family: 'Oswald', sans-serif;
-  }
-
-  .carousel-action .btn-primary {
-    border-color: black;
-    background: none;
-    color: #000;
-    transition-property: all;
-    transition-duration: 1000ms;
-  }
-
-  .carousel-action .btn-primary:hover {
-    background: black;
-    color: white;
-    border-color: black;
-  }
-
-  .carousel-action .btn-success {
-    background: #8fc93c;
-    border: none;
-  }
-
-  .carousel-action .btn-success:hover {
-    background: #87bd35;
-  }
-
-  .carousel-indicators li,
-  .carousel-indicators li.active {
-    width: 11px;
-    height: 11px;
-    border-radius: 50%;
-    margin: 1px 6px;
-  }
-
-  .carousel-indicators li {
-    background: transparent;
-    border: 1px solid #fff;
-  }
-
-  .carousel-indicators li.active {
-    background: black;
-    border-color: black;
-  }
+}
+.carousel-caption h3, .carousel-caption p {
+	color: #fff;
+	display: inline-block;
+	font-family: 'Oswald', sans-serif;
+	text-shadow: none;
+	margin-bottom: 20px;
+}
+.carousel-caption h3 {
+	background: rgba(0,0,0,0.9);
+	padding: 12px 24px;
+	font-size: 40px;	
+	text-transform: uppercase;
+}
+.carousel-caption p {
+	background: black;
+	padding: 10px 20px;
+	font-size: 20px;
+	font-weight: 300;
+}
+.carousel-action {
+	padding: 10px 0 30px;
+}
+.carousel-action .btn {
+	min-height: 34px;
+	border-radius: 3px;
+	margin: 3px;
+	min-width: 150px;
+	text-transform: uppercase;
+	font-family: 'Oswald', sans-serif;
+}
+.carousel-action .btn-primary {
+	border-color: black;
+	background: none;
+	color: #000;
+  transition-property: all;
+  transition-duration: 1000ms;
+}
+.carousel-action .btn-primary:hover {
+	background: black;
+	color: white;
+  border-color:black;
+}
+.carousel-action .btn-success {
+	background: #8fc93c;
+	border: none;
+}
+.carousel-action .btn-success:hover {
+	background: #87bd35;
+}
+.carousel-indicators li, .carousel-indicators li.active {
+	width: 11px;
+	height: 11px;
+	border-radius: 50%;
+	margin: 1px 6px;
+}
+.carousel-indicators li {
+	background: transparent;
+	border: 1px solid #fff;
+}
+.carousel-indicators li.active {
+	background: black;
+	border-color: black;
+}
 </style>
 
 
@@ -132,122 +112,121 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <?php include 'header.php'; ?>
-<section class="home-about-section">
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg">
-        <div class="row">
-          <p class="col-12 title scaled-p">
-            Dijital çözüm ortağınız </p>
-          <div class="col-12 description">
-            Web Site Yapımı, Mobil Uygulama Yapımı, SEO, Sosyal Medya Yönetimi, Dijital Tasarım gibi alanlardaki
-            ihtiyaçlarınız için buradayız.</div>
-          <div class="col-12 mt-5">
-            <!--
+ <section class="home-about-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg">
+          <div class="row">
+            <p class="col-12 title scaled-p">
+           Dijital çözüm ortağınız </p>
+            <div class="col-12 description">
+            Web Site Yapımı, Mobil Uygulama Yapımı, SEO, Sosyal Medya Yönetimi, Dijital Tasarım gibi alanlardaki ihtiyaçlarınız için buradayız.</div>
+            <div class="col-12 mt-5">
+              <!--
               <a href="" class="button button-gray-black">
                 <i class="fad fa-chevron-right button-before"></i>Detaylı İncele </a>
               -->
+              </div>
+            </div>
+          </div>
+          <div class="col-auto d-none d-lg-block ml-auto mr-auto image-container">
+            <div class="row">
+              <div class="col-4 ml-auto mr-auto image">
+                
+              </div>
+            </div>
+            <div class="row align-items-center">
+              <div class="col-12">
+                <img src="assets/img/giphy2.gif" style="border-radius: 10px;width:500px;">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-4 ml-auto mr-auto image">
+               
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="col-auto d-none d-lg-block ml-auto mr-auto image-container">
-        <div class="row">
-          <div class="col-4 ml-auto mr-auto image">
-
-          </div>
-        </div>
-        <div class="row align-items-center">
-          <div class="col-12">
-            <img src="assets/img/giphy2.gif" style="border-radius: 10px;width:500px;">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4 ml-auto mr-auto image">
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+    </section>
 <section>
-  <div class="row">
-    <div class="col-md-12">
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Carousel indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <!-- Wrapper for carousel items -->
-        <div class="carousel-inner">
-          <div class="item active">
-            <img src="assets/images/tablet.jpg" alt="">
-            <div class="carousel-caption">
-              <h3>Dijital web tasarım ve reklam stüdyosu</h3>
-              <p>Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget cursus dolor.</p>
-              <div class="carousel-action">
-                <a href="#" class="btn btn-primary">İletişim</a>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <img src="assets/images/slider2.webp" alt="">
-            <div class="carousel-caption">
-              <h3>Amazing Digital Experience</h3>
-              <p>Nullam hendrerit justo non leo aliquet imperdiet. Etiam sagittis lectus condime dapibus.</p>
-              <div class="carousel-action">
-                <a href="#" class="btn btn-primary">İletişim</a>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <img src="assets/images/slider3.jpg" alt="">
-            <div class="carousel-caption">
-              <h3>Live Monitoring Tools</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu pellentesque sem tempor.</p>
-              <div class="carousel-action">
-                <a href="#" class="btn btn-primary">İletişim</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Carousel controls -->
-        <a class="carousel-control left" href="#myCarousel" data-slide="prev">
-          <i class="fa fa-angle-left"></i>
-        </a>
-        <a class="carousel-control right" href="#myCarousel" data-slide="next">
-          <i class="fa fa-angle-right"></i>
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="row">
+		<div class="col-md-12">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Carousel indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>   
+				<!-- Wrapper for carousel items -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="assets/images/tablet.jpg" alt="">
+						<div class="carousel-caption">
+							<h3>Dijital web tasarım ve reklam stüdyosu</h3>
+							<p>Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget cursus dolor.</p>
+							<div class="carousel-action">
+								<a href="#" class="btn btn-primary">İletişim</a>
+							</div>
+						</div>
+					</div>	
+					<div class="item">
+						<img src="assets/images/slider2.webp" alt="">
+						<div class="carousel-caption">
+							<h3>Amazing Digital Experience</h3>							
+							<p>Nullam hendrerit justo non leo aliquet imperdiet. Etiam sagittis lectus condime dapibus.</p>
+							<div class="carousel-action">
+              <a href="#" class="btn btn-primary">İletişim</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<img src="assets/images/slider3.jpg" alt="">
+						<div class="carousel-caption">
+							<h3>Live Monitoring Tools</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu pellentesque sem tempor.</p>
+							<div class="carousel-action">
+              <a href="#" class="btn btn-primary">İletişim</a>
+							</div>
+						</div>
+					</div>	
+				</div>
+				<!-- Carousel controls -->
+				<a class="carousel-control left" href="#myCarousel" data-slide="prev">
+					<i class="fa fa-angle-left"></i>
+				</a>
+				<a class="carousel-control right" href="#myCarousel" data-slide="next">
+					<i class="fa fa-angle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
 </section>
 
-<section class="references-home-section" style="margin-bottom:-120px;">
-  <div class="container">
-    <div class="row top">
-      <p class="col col-mr-auto title scaled-p">Projelerimiz</p>
-    </div>
-    <!-- item -->
-    <div class="row">
-      <!-- item -->
-      <div class="col-6 col-lg-6 col-xxl-6 reference-list-col">
-        <div class="reference-list-container" style="background-color: #0e2a4f">
-          <p class="title">
-            <a href="">
-              GoMekan
-            </a>
-          </p>
-          <div class="row">
-            <div class="col-6 col-lg-6 col-xxl-6">
-              <div class="description">
-                GoMekan web sitesi ve mobil uygulaması geliştirildi, kullanıma açıldı.
-              </div>
-            </div>
-            <div class="col-6 col-lg-6 col-xxl-6">
-              <div class="image">
+    <section class="references-home-section" style="margin-bottom:-120px;">
+      <div class="container">
+        <div class="row top">
+          <p class="col col-mr-auto title scaled-p">Projelerimiz</p>
+        </div>
+        <!-- item -->
+        <div class="row">
+          <!-- item -->
+          <div class="col-6 col-lg-6 col-xxl-6 reference-list-col">
+            <div class="reference-list-container" style="background-color: #0e2a4f">
+              <p class="title">
+                <a href="">
+                    GoMekan
+                </a>
+              </p>
+              <div class="row">
+                <div class="col-6 col-lg-6 col-xxl-6">
+                    <div class="description">
+                    GoMekan web sitesi ve mobil uygulaması geliştirildi, kullanıma açıldı.
+                  </div>
+                </div>
+                <div class="col-6 col-lg-6 col-xxl-6">
+                <div class="image">
                 <a href="gomekan.com">
                   <picture class="img-fluid">
                     <!--
@@ -256,26 +235,26 @@
                   </picture>
                 </a>
               </div>
+                </div>
+              </div>
+              
             </div>
           </div>
-
-        </div>
-      </div>
-      <div class="col-6 col-lg-6 col-xxl-6 reference-list-col">
-        <div class="reference-list-container" style="background-color: #0e2a4f">
-          <p class="title">
-            <a href="">
-              GoMekan
-            </a>
-          </p>
-          <div class="row">
-            <div class="col-6 col-lg-6 col-xxl-6">
-              <div class="description">
-                GoMekan web sitesi ve mobil uygulaması geliştirildi, kullanıma açıldı.
-              </div>
-            </div>
-            <div class="col-6 col-lg-6 col-xxl-6">
-              <div class="image">
+          <div class="col-6 col-lg-6 col-xxl-6 reference-list-col">
+            <div class="reference-list-container" style="background-color: #0e2a4f">
+              <p class="title">
+                <a href="">
+                    GoMekan
+                </a>
+              </p>
+              <div class="row">
+                <div class="col-6 col-lg-6 col-xxl-6">
+                    <div class="description">
+                    GoMekan web sitesi ve mobil uygulaması geliştirildi, kullanıma açıldı.
+                  </div>
+                </div>
+                <div class="col-6 col-lg-6 col-xxl-6">
+                <div class="image">
                 <a href="gomekan.com">
                   <picture class="img-fluid">
                     <!--
@@ -284,14 +263,14 @@
                   </picture>
                 </a>
               </div>
+                </div>
+              </div>
+              
             </div>
           </div>
-
+           
         </div>
-      </div>
-
-    </div>
-    <!--
+      <!--
         <div class="row">
           <div class="col-auto mr-auto ml-auto work-stages-button">
             <a href="projelerimiz.html" class="button button-pink-white">
