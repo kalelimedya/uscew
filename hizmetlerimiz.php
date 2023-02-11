52056e13cbf12be5fe5e73f7e82b4a16bca44465
YigithanGumus <yigithangumuss@gmail.com>
2023-02-12 00:43:08 +0300

    aa
    aaa

--- hizmetlerimiz.php
+++ hizmetlerimiz.php
@@ -1,66 +1,132 @@
 <?php include 'header.php'; ?>
-<div class="container">
-    <section class="working-process-section">
-        <div class="container">
-            <div class="row">
-                <p class="col-12 scaled-p">3 Adımda<br>
-                    çalışma sürecimiz.</p>
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-12 col-md-4 working-process-col">
-                            <div class="working-process-container">
-                                <div class="row">
-                                    <div class="col-12 image">
-                                        <img src="https://www.kumsalajans.com/assets/images/work-process-1.svg"
-                                            class="img-fluid" alt="project.workProcess.1.title">
-                                    </div>
-                                    <div class="col-12 title">
-                                        Keşif ve Planlama </div>
-                                    <div class="col-12 description">
-                                        Strateji planlama ile istenilen işin anlaşılması ve detaylandırılması sağlanır.
-                                    </div>
+<section class="working-process-section">
+    <div class="container">
+        <div class="row">
+            <p class="col-12 scaled-p">Hizmetlerimiz</p>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12 col-md-4 working-process-col">
+                        <div class="working-process-container">
+                            <div class="row">
+                                <div class="col-12 image">
+                                    <img src="assets/img/website-icon-22.png" class="img-fluid"
+                                        style="width:110px;height:110px;" alt="web dizayn">
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 working-process-col">
-                            <div class="working-process-container">
-                                <div class="row">
-                                    <div class="col-12 image">
-                                        <img src="https://www.kumsalajans.com/assets/images/work-process-2.svg"
-                                            class="img-fluid" alt="project.workProcess.2.title">
-                                    </div>
-                                    <div class="col-12 title">
-                                        Tasarım ve Geliştirme </div>
-                                    <div class="col-12 description">
-                                        Belirlenen strateji ile tasarımın ve yazılımın oluşturulması sağlanır. </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 working-process-col">
-                            <div class="working-process-container">
-                                <div class="row">
-                                    <div class="col-12 image">
-                                        <img src="https://www.kumsalajans.com/assets/images/work-process-3.svg"
-                                            class="img-fluid" alt="project.workProcess.3.title">
-                                    </div>
-                                    <div class="col-12 title">
-                                        Test ve Yayına Alma </div>
-                                    <div class="col-12 description">
-                                        Tasarımın ve yazılımın detaylı olarak incelenmesi ve kontroller sonrasında size
-                                        teslim edilerek yayına alınması sağlanır. </div>
+                                <div class="col-12 title">
+                                    Web Dizayn </div>
+                                <div class="col-12 description">
+                                    WordPress, Html, CSS, PHP yazılım çözümleri ile web sitesi projenizi USCEW ekibi
+                                    olarak profesyonel ve 7/24 canlı desteğimizle sizlere yardımcı olmaktan memnuniyet
+                                    duyarız.
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-auto mr-auto ml-auto work-stages-button">
-                            <a href="https://kumsalajans.com/calisma-asamalari" class="button button-pink-white">
-                                <i class="fad fa-chevron-double-right button-before"></i>Detaylı İncele </a>
+                    <div class="col-12 col-md-4 working-process-col">
+                        <div class="working-process-container">
+                            <div class="row">
+                                <div class="col-12 image">
+                                    <img src="assets/img/img_557408.png" class="img-fluid"
+                                        style="width:150px;height:110px;" alt="yazılım">
+                                </div>
+                                <div class="col-12 title">
+                                    YAZILIM</div>
+                                <div class="col-12 description">
+                                    USCEW olarak imza atmış olduğumuz 5 adet projemizde yazılım, teknoloi ve vizyonu bir
+                                    araya getirdik. Gomekan, Jobsangel, Shopsangel, Egg it Up ve Number Cracker ile
+                                    endüstriye yön vermeye devam ediyoruz.Hayalinizdeki projelere USCEW ekibi olarak
+                                    yardımcı olmaktan memnuniyet duyarız.Belirlenen strateji ile tasarımın ve yazılımın
+                                    oluşturulması sağlanır.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 working-process-col">
+                        <div class="working-process-container">
+                            <div class="row">
+                                <div class="col-12 image">
+                                    <img src="assets/img/5902216.png" class="img-fluid" alt="seo"
+                                        style="width:110px;height:110px;">
+                                </div>
+                                <div class="col-12 title">
+                                    SEO </div>
+                                <div class="col-12 description">
+                                    İnternet sitenize reklam vermeden trafik çekin. Kazancınızı arttırın. USCEW olarak
+                                    profesyonel SEO hizmetimizle sitenizi arama motorunda istediğiniz kelimelerde ilk
+                                    sayfaya getiriyoruz. Bu sayede sitenizde başarıyı kaçınılmaz kılıyoruz.</div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="row" style="margin-top:30px;">
+                    <div class="col-12 col-md-4 working-process-col">
+                        <div class="working-process-container">
+                            <div class="row">
+                                <div class="col-12 image">
+                                    <img src="assets/img/191.png" class="img-fluid"
+                                        style="width:110px;height:110px;" alt="mobil uygulama">
+                                </div>
+                                <div class="col-12 title">
+                                    Mobil Uygulama </div>
+                                <div class="col-12 description">
+                                    USCEW olarak iOS ve Android’de hayalinizdeki mobil uygulamayı profesyonel ekibimizle
+                                    hayata geçiriyoruz. Mobil uygulamanızı kendiniz yayınlayabilir veya USCEW markası
+                                    altında pazara sunabilirsiniz.
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 working-process-col">
+                        <div class="working-process-container">
+                            <div class="row">
+                                <div class="col-12 image">
+                                    <img src="assets/img/digital-design-3.png" class="img-fluid"
+                                        style="width:110px;height:110px;"
+                                        alt="dijital tasarım">
+                                </div>
+                                <div class="col-12 title">
+                                    Dijital Tasarım</div>
+                                <div class="col-12 description">
+                                    Tüm dijital tasarım ihtiyaçlarınızı USCEW’in profesyonel tasarımcıları ve editörleri
+                                    ile hayata geçirebilirsiniz.
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 working-process-col">
+                        <div class="working-process-container">
+                            <div class="row">
+                                <div class="col-12 image">
+                                    <img src="assets/img/197-1978380_digital-marketing-icon-black.png"
+                                        class="img-fluid" alt="pazarlama" style="width:110px;height:110px;">
+                                </div>
+                                <div class="col-12 title">
+                                    Pazarlama</div>
+                                <div class="col-12 description">
+                                    Ekibimizde bulunan pazarlama uzmanları ile iş modelinizi oluşturup işinize ve
+                                    girişiminize pazarlama çözümleri sunuyoruz. Bu sayede başarınızı kaçınılmaz
+                                    kılıyoruz.
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!--
+                    <div class="col-auto mr-auto ml-auto work-stages-button">
+                        <a href="" class="button button-pink-white">
+                            <i class="fad fa-chevron-double-right button-before"></i>Detaylı İncele </a>
+                    </div>
+                    -->
+                </div>
             </div>
         </div>
-    </section>
-</div>
+
+    </div>
+</section>
 <?php include 'footer.php'; ?>
