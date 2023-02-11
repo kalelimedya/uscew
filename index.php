a0fbc8ad0eebbf907faf52620c4fb286cf4e3e83
erenekmekci <erenekm@gmail.com>
2023-02-11 17:17:31 +0300

    deneme

--- index.php
+++ index.php
@@ -1,3 +1,113 @@
+
+<style>
+.carousel{
+	margin: 30px 0;
+    background: #ccc;
+  	position: relative;
+    box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+}
+.carousel:after {
+	content: "";
+    position: absolute;
+    z-index: -1;
+    box-shadow: 0 0 20px rgba(0,0,0,0.8);
+    height: 60px;
+    bottom: 0px;
+    left: 10px;
+    right: 10px;
+    border-radius: 100px / 20px;
+}
+.carousel .item {
+    text-align: center;
+	overflow: hidden;
+    height: 475px;
+}
+.carousel .item img {
+	max-width: 100%;
+    margin: 0 auto; /* Align slide image horizontally center in Bootstrap v3 */
+}
+.carousel .carousel-control {
+	width: 50px;
+    height: 50px;
+    background: #000;
+    margin: auto 0;
+	opacity: 0.8;
+}
+.carousel .carousel-control:hover {
+	opacity: 0.9;
+}
+.carousel .carousel-control i {
+    font-size: 41px;
+}
+.carousel-caption h3, .carousel-caption p {
+	color: #fff;
+	display: inline-block;
+	font-family: 'Oswald', sans-serif;
+	text-shadow: none;
+	margin-bottom: 20px;
+}
+.carousel-caption h3 {
+	background: rgba(0,0,0,0.9);
+	padding: 12px 24px;
+	font-size: 40px;	
+	text-transform: uppercase;
+}
+.carousel-caption p {
+	background: #8fc93c;
+	padding: 10px 20px;
+	font-size: 20px;
+	font-weight: 300;
+}
+.carousel-action {
+	padding: 10px 0 30px;
+}
+.carousel-action .btn {
+	min-height: 34px;
+	border-radius: 0;
+	margin: 3px;
+	min-width: 150px;
+	text-transform: uppercase;
+	font-family: 'Oswald', sans-serif;
+}
+.carousel-action .btn-primary {
+	border-color: #000;
+	background: none;
+	color: #000;
+}
+.carousel-action .btn-primary:hover {
+	background: #000;
+	color: #fff;
+}
+.carousel-action .btn-success {
+	background: #8fc93c;
+	border: none;
+}
+.carousel-action .btn-success:hover {
+	background: #87bd35;
+}
+.carousel-indicators li, .carousel-indicators li.active {
+	width: 11px;
+	height: 11px;
+	border-radius: 50%;
+	margin: 1px 6px;
+}
+.carousel-indicators li {
+	background: transparent;
+	border: 1px solid #fff;
+}
+.carousel-indicators li.active {
+	background: #8fc93c;
+	border-color: #8fc93c;
+}
+</style>
+
+
+<link href="https://fonts.googleapis.com/css?family=Roboto|Oswald:300,400" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <?php include 'header.php'; ?>
  <section class="home-about-section">
     <div class="container">
@@ -36,46 +146,63 @@
         </div>
       </div>
     </section>
-<section class="home-slider-section" style="margin-bottom:-20px;">
-  <div class="container">
-    <div class="row">
-      
-      <div class="col-12 col-sm-7 col-md-6 col-lg-7 col-xxl-6 image-col">
-        <div>
-          <picture class="img-fluid">
-            <img sizes="650px" src="assets/images/SEO.gif" class="img-fluid" alt="slider">
-          </picture>
-        </div>
-        </div>
-        <div class="col-12 col-sm-5 col-md-6 col-lg-5 col-xxl-6 slider-col">
-
-        <div class="owl-carousel" id="home-slider-owl-carousel">
-
-          <div class="item">
-            <p class="scaled-p">Dijital web tasarım ve reklam stüdyosu</p>
-          </div>
-          <div class="item">
-            <p class="scaled-p">Dijital web tasarım</p>
-          </div>
-          <div class="item">
-            <p class="scaled-p">Dijital </p>
-          </div>
-          <div class="item">
-            <p class="scaled-p">Dijital web tasarım </p>
-          </div>
-        </div>
-       <div class="owl-nav owl-fa-nav">
-          <a href="#" class="owl-prev">
-            <i class="fad fa-chevron-left"></i>
-          </a>
-          <a href="#" class="owl-next">
-            <i class="fad fa-chevron-right"></i>
-          </a>
-        </div>
-      </div>
-      </div>
-    </div>
-  </section>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Carousel indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>   
+				<!-- Wrapper for carousel items -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="assets/images/tablet.jpg" alt="">
+						<div class="carousel-caption">
+							<h3>Dijital web tasarım ve reklam stüdyosu</h3>
+							<p>Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget cursus dolor.</p>
+							<div class="carousel-action">
+								<a href="#" class="btn btn-primary">Learn More</a>
+								<a href="#" class="btn btn-success">Try Now</a>
+							</div>
+						</div>
+					</div>	
+					<div class="item">
+						<img src="/examples/images/slides/tablet.jpg" alt="">
+						<div class="carousel-caption">
+							<h3>Amazing Digital Experience</h3>							
+							<p>Nullam hendrerit justo non leo aliquet imperdiet. Etiam sagittis lectus condime dapibus.</p>
+							<div class="carousel-action">
+								<a href="#" class="btn btn-primary">Learn More</a>
+								<a href="#" class="btn btn-success">Try Now</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<img src="/examples/images/slides/workstation.jpg" alt="">
+						<div class="carousel-caption">
+							<h3>Live Monitoring Tools</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu pellentesque sem tempor.</p>
+							<div class="carousel-action">
+								<a href="#" class="btn btn-primary">Learn More</a>
+								<a href="#" class="btn btn-success">Try Now</a>
+							</div>
+						</div>
+					</div>	
+				</div>
+				<!-- Carousel controls -->
+				<a class="carousel-control left" href="#myCarousel" data-slide="prev">
+					<i class="fa fa-angle-left"></i>
+				</a>
+				<a class="carousel-control right" href="#myCarousel" data-slide="next">
+					<i class="fa fa-angle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
 
     <section class="references-home-section" style="margin-bottom:-120px;">
       <div class="container">
