b5a4b483fad293bc18f928e3d98e7aae590b068e
erenekmekci <erenekm@gmail.com>
2023-02-11 16:10:55 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,107 +1,281 @@
-<footer class="footer">
-              <div class="container">
-                <div class="row info-container">
-                  <div class="col-12 col-lg-auto social-container">
-                    <a href="https://www.facebook.com/ajanskumsal" rel="noreferrer" target="_blank" class="fab fa-facebook" aria-label="facebook"></a>
-                    <a href="https://twitter.com/ajanskumsal" rel="noreferrer" target="_blank" class="fab fa-twitter" aria-label="twitter"></a>
-                    <a href="https://www.instagram.com/kumsalajans/" rel="noreferrer" target="_blank" class="fab fa-instagram" aria-label="instagram"></a>
-                    <a href="https://www.linkedin.com/company/kumsalajans/" rel="noreferrer" target="_blank" class="fab fa-linkedin" aria-label="linkedin"></a>
-                  </div>
-                </div>
+<style>
+
+ul {
+    margin: 0px;
+    padding: 0px;
+}
+.footer-section {
+  background: #151414;
+  position: relative;
+}
+.footer-cta {
+  border-bottom: 1px solid #373636;
+}
+.single-cta i {
+  color: #ff5e14;
+  font-size: 30px;
+  float: left;
+  margin-top: 8px;
+}
+.cta-text {
+  padding-left: 15px;
+  display: inline-block;
+}
+.cta-text h4 {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 2px;
+}
+.cta-text span {
+  color: #757575;
+  font-size: 15px;
+}
+.footer-content {
+  position: relative;
+  z-index: 2;
+}
+.footer-pattern img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 330px;
+  background-size: cover;
+  background-position: 100% 100%;
+}
+.footer-logo {
+  margin-bottom: 30px;
+}
+.footer-logo img {
+    max-width: 200px;
+}
+.footer-text p {
+  margin-bottom: 14px;
+  font-size: 14px;
+      color: #7e7e7e;
+  line-height: 28px;
+}
+.footer-social-icon span {
+  color: #fff;
+  display: block;
+  font-size: 20px;
+  font-weight: 700;
+  font-family: 'Poppins', sans-serif;
+  margin-bottom: 20px;
+}
+.footer-social-icon a {
+  color: #fff;
+  font-size: 16px;
+  margin-right: 15px;
+}
+.footer-social-icon i {
+  height: 40px;
+  width: 40px;
+  text-align: center;
+  line-height: 38px;
+  border-radius: 50%;
+}
+.facebook-bg{
+  background: #3B5998;
+}
+.twitter-bg{
+  background: #55ACEE;
+}
+.google-bg{
+  background: #DD4B39;
+}
+.footer-widget-heading h3 {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 40px;
+  position: relative;
+}
+.footer-widget-heading h3::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -15px;
+  height: 2px;
+  width: 50px;
+  background: #ff5e14;
+}
+.footer-widget ul li {
+  display: inline-block;
+  float: left;
+  width: 50%;
+  margin-bottom: 12px;
+}
+.footer-widget ul li a:hover{
+  color: #ff5e14;
+}
+.footer-widget ul li a {
+  color: #878787;
+  text-transform: capitalize;
+}
+.subscribe-form {
+  position: relative;
+  overflow: hidden;
+}
+.subscribe-form input {
+  width: 100%;
+  padding: 14px 28px;
+  background: #2E2E2E;
+  border: 1px solid #2E2E2E;
+  color: #fff;
+}
+.subscribe-form button {
+    position: absolute;
+    right: 0;
+    background: #ff5e14;
+    padding: 13px 20px;
+    border: 1px solid #ff5e14;
+    top: 0;
+}
+.subscribe-form button i {
+  color: #fff;
+  font-size: 22px;
+  transform: rotate(-6deg);
+}
+.copyright-area{
+  background: #202020;
+  padding: 25px 0;
+}
+.copyright-text p {
+  margin: 0;
+  font-size: 14px;
+  color: #878787;
+}
+.copyright-text p a{
+  color: #ff5e14;
+}
+.footer-menu li {
+  display: inline-block;
+  margin-left: 20px;
+}
+.footer-menu li:hover a{
+  color: #ff5e14;
+}
+.footer-menu li a {
+  font-size: 14px;
+  color: #878787;
+}
+
+</style>
+
+<footer class="footer-section">
+        <div class="container">
+            <div class="footer-cta pt-5 pb-5">
                 <div class="row">
-                  <div class="col-12 blog-title">
-                    <p class="scaled-p">Blog Başlıkları</p>
-                  </div>
-                  <div class="col-12">
-                    <div class="row blog-container">
-                      <div class="col-12 col-md-6 col-lg-4">
-                        <a href="https://kumsalajans.com/blog/web-tasarim/history-of-web-design" title="History Of Web Design">
-                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">History Of Web Design</span>
-                        </a>
-                      </div>
-                      <div class="col-12 col-md-6 col-lg-4">
-                        <a href="https://kumsalajans.com/blog/web-tasarim/what-is-web-design" title="What is web design?">
-                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">What is web design?</span>
-                        </a>
-                      </div>
-                      <div class="col-12 col-md-6 col-lg-4">
-                        <a href="https://kumsalajans.com/blog/dijital-pazarlama/sosyal-medya-ajansi-nedir" title="Sosyal Medya Ajansları">
-                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Sosyal Medya Ajansları</span>
-                        </a>
-                      </div>
-                      <div class="col-12 col-md-6 col-lg-4">
-                        <a href="https://kumsalajans.com/blog/web-tasarim/web-sitesi-yenileme-nedir" title="Web Sitesi Yenileme Nedir? | Hangi Durumlarda Yapılabilir?">
-                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Web Sitesi Yenileme Nedir? | Hangi Durumlarda Yapılabilir?</span>
-                        </a>
-                      </div>
-                      <div class="col-12 col-md-6 col-lg-4">
-                        <a href="https://kumsalajans.com/blog/dijital-pazarlama/dijital-pazarlama-ajansi-nedir" title="Dijital Pazarlama Ajansı Nedir? | Hizmetleri Nelerdir?">
-                          <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Dijital Pazarlama Ajansı Nedir? | Hizmetleri Nelerdir?</span>
-                        </a>
-                      </div>
-                      <div class="col-12 col-md-6 col-lg-4">
-                        <a href="https://kumsalajans.com/blog/dijital-pazarlama/instagram-reklam-danismanligi-nedir" title="Instagram Reklam Danışmanlığı Nedir? | Neleri Kapsar?">
-                         <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Instagram Reklam Danışmanlığı Nedir? | Neleri Kapsar?</span>
-                       </a>
-                     </div>
-                     <div class="col-12 col-md-6 col-lg-4">
-                      <a href="https://kumsalajans.com/blog/dijital-pazarlama/reklam-ajansi-nedir" title="Reklam Ajansı Nedir? | Neden Gereklidir?">
-                        <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Reklam Ajansı Nedir? | Neden Gereklidir?</span>
-                      </a>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="cta-text">
+                                <h4>Find us</h4>
+                                <span>1010 Avenue, sw 54321, chandigarh</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-4">
-                      <a href="https://kumsalajans.com/blog/dijital-pazarlama/google-reklam-danismanligi-nedir" title="Google Reklam Danışmanlığı Nedir? | Neden Önemlidir?">
-                        <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">Google Reklam Danışmanlığı Nedir? | Neden Önemlidir?</span>
-                      </a>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="fas fa-phone"></i>
+                            <div class="cta-text">
+                                <h4>Call us</h4>
+                                <span>9876543210 0</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-4">
-                      <a href="https://kumsalajans.com/blog/web-ve-mobil-yazilim/nft-game-nedir" title="NFT Game Nedir? | NFT Game Gelişimi">
-                        <i class="fad fa-chevron-double-right mr-1"></i><span class="hover-border">NFT Game Nedir? | NFT Game Gelişimi</span>
-                      </a>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="far fa-envelope-open"></i>
+                            <div class="cta-text">
+                                <h4>Mail us</h4>
+                                <span>mail@info.com</span>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
-              <div class="row copyright">
-                <div class="col-sm">
-                  Telif Hakkı © Uscew 2023. <span>Tüm Hakları Saklıdır.</span>
-                </div>
-                <div class="col-auto mt-3 mt-sm-0">
-                  
-                </div>
-              </div>
             </div>
-            <a href="https://wa.me/+908503037441" class="fixed-whatsapp">
-              <img src="storage/media/1463/whatsapp.svg" alt="">
-            </a>
-          </footer>
-          <div class="fixed-buttons">
-            <div class="row no-gutters">
-              <div class="col-12 d-block d-md-none">
-                
-              </div>
-              <div class="yukari col-12 d-none d-md-block">
-                <a href="javascript:void(0);" class="scroll-top-body up">
-                  <i class="fad fa-chevron-up"></i>
-                </a>
-              </div>
+            <div class="footer-content pt-5 pb-5">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-logo">
+                                <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" class="img-fluid" alt="logo"></a>
+                            </div>
+                            <div class="footer-text">
+                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                elit,Lorem ipsum dolor sit amet.</p>
+                            </div>
+                            <div class="footer-social-icon">
+                                <span>Follow us</span>
+                                <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                                <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                                <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Useful Links</h3>
+                            </div>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">about</a></li>
+                                <li><a href="#">services</a></li>
+                                <li><a href="#">portfolio</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Our Services</a></li>
+                                <li><a href="#">Expert Team</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">Latest News</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Subscribe</h3>
+                            </div>
+                            <div class="footer-text mb-25">
+                                <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                            </div>
+                            <div class="subscribe-form">
+                                <form action="#">
+                                    <input type="text" placeholder="Email Address">
+                                    <button><i class="fab fa-telegram-plane"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="cookie-container d-none">
+        </div>
+        <div class="copyright-area">
             <div class="container">
-              <div class="row align-items-center">
-                <div class="col">
-                   </div>
-                  <div class="col-auto">
-                    <a href="#" class="button button-gray-black cookie-accept">
-                      <span class="d-sm-none"><i class="fad fa-times"></i></span>
-                      <span class="d-none d-sm-flex align-items-center"><i class="fad fa-chevron-double-right mr-1"></i> Kabul Et</span>
-                    </a>
-                  </div>
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                        <div class="copyright-text">
+                            <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Terms</a></li>
+                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#">Policy</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-                <script type="text/javascript" src="assets/front/js/js.js"></script>
+        </div>
+    </footer>
 
               </body>
               </html>
