0dde68c608e63d5ff16c1df16c3a1991f86c4b66
YigithanGumus <yigithangumuss@gmail.com>
2023-02-15 02:14:00 +0300

    a

--- action.php
+++ action.php
@@ -23,45 +23,7 @@ if (isset($_POST['submit_btn'])) {
         VALUES ('$name', '$email', '$business','$message','$project','$tel')";
         
                 if ($con->multi_query($sql) == TRUE) {
-                    
-                               require 'PHPMailer/src/Exception.php';
-                                require 'PHPMailer/src/PHPMailer.php';
-                                require 'PHPMailer/src/SMTP.php';
-                                
-                                
-                                $mail = new PHPMailer(true);
-                                try {
-                                //Server settings
-                                $mail->CharSet = 'UTF-8';
-                                $mail->SMTPDebug = 0; // debug on - off
-                                $mail->isSMTP(); 
-                                $mail->Host = 'smtp.gmail.com'; // SMTP sunucusu örnek : mail.alanadi.com
-                                $mail->SMTPAuth = true; // SMTP Doğrulama
-                                $mail->Username = 'uscewyazilim@gmail.com'; // Mail kullanıcı adı
-                                $mail->Password = 'jtddwpcezgvefakz'; // Mail şifresi
-                                $mail->SMTPSecure = 'ssl'; // Şifreleme
-                                $mail->Port = 465; // SMTP Port
-                                $mail->SMTPOptions = array(
-                                'ssl' => array(
-                                'verify_peer' => false,
-                                'verify_peer_name' => false,
-                                'allow_self_signed' => true
-                                )
-                                );
-                                
-                                //Alıcılar
-                                $mail->setfrom('uscewyazilim@gmail.com', 'Soru Sorun&Teklif Al');
-                                $mail->addAddress('uscewyazilim@gmail.com');
-                                $mail->addReplyTo('uscewyazilim@gmail.com');
-                                //İçerik
-                                $mail->isHTML(true);
-                                $mail->Subject = 'Proje:'.$_POST['project'];
-                                $mail->Body ="İsim ve Soyadı:".$_POST['name']."<br>".  "Mesaj:".$_POST['message']."<br> <br>"."Telefon Numarası:".$_POST['tel']."<br>". "Proje:".$_POST['project']."<br>"."Şirket:".$_POST['business'];
-                                if ($mail->send()) {
-                                    header("location:iletisim.php?durum=ok");
-                                } else {
-                                    header("location:iletisim.php?durum=no");
-                                }
+                    header("Location:index.php");
 
 
                 } else {
