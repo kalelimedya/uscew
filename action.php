9837d0ebcbd9cdbc71cbc01d0ebdfce65ac135da
YigithanGumus <yigithangumuss@gmail.com>
2023-02-15 02:07:38 +0300

    a

--- action.php
+++ action.php
@@ -24,37 +24,45 @@ if (isset($_POST['submit_btn'])) {
         
                 if ($con->multi_query($sql) === TRUE) {
                     
-                        //Create an instance; passing `true` enables exceptions
-                        $mail = new PHPMailer(true);
+                               require 'PHPMailer/src/Exception.php';
+                                require 'PHPMailer/src/PHPMailer.php';
+                                require 'PHPMailer/src/SMTP.php';
+                                
+                                
+                                $mail = new PHPMailer(true);
+                                try {
+                                //Server settings
+                                $mail->CharSet = 'UTF-8';
+                                $mail->SMTPDebug = 0; // debug on - off
+                                $mail->isSMTP(); 
+                                $mail->Host = 'smtp.gmail.com'; // SMTP sunucusu örnek : mail.alanadi.com
+                                $mail->SMTPAuth = true; // SMTP Doğrulama
+                                $mail->Username = 'uscewyazilim@gmail.com'; // Mail kullanıcı adı
+                                $mail->Password = 'jtddwpcezgvefakz'; // Mail şifresi
+                                $mail->SMTPSecure = 'ssl'; // Şifreleme
+                                $mail->Port = 465; // SMTP Port
+                                $mail->SMTPOptions = array(
+                                'ssl' => array(
+                                'verify_peer' => false,
+                                'verify_peer_name' => false,
+                                'allow_self_signed' => true
+                                )
+                                );
+                                
+                                //Alıcılar
+                                $mail->setfrom('uscewyazilim@gmail.com', 'Soru Sorun&Teklif Al');
+                                $mail->addAddress('uscewyazilim@gmail.com');
+                                $mail->addReplyTo('uscewyazilim@gmail.com');
+                                //İçerik
+                                $mail->isHTML(true);
+                                $mail->Subject = 'Proje:'.$_POST['project'];
+                                $mail->Body ="İsim ve Soyadı:".$_POST['name']."<br>".  "Mesaj:".$_POST['message']."<br> <br>"."Telefon Numarası:".$_POST['tel']."<br>". "Proje:".$_POST['project']."<br>"."Şirket:".$_POST['business'];
+                                $mail->send();
+                                echo "Mesajınız İletildi --> ".$_POST['mail']."<br>";
+                                } catch (Exception $e) {
+                                echo 'Mesajınız İletilemedi. Hata: ', $mail->ErrorInfo;
+                                }
 
-                        try {
-                            //Server settings
-                            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                            $mail->isSMTP();                                            //Send using SMTP
-                            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                            $mail->Username   = 'uscew@gmail.com';                     //SMTP username
-                            $mail->Password   = 'jtddwpcezgvefakz';                               //SMTP password
-                            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-                            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                            //Recipients
-                            $mail->setFrom('uscew@gmail.com', 'Mailer');
-                            $mail->addAddress('uscew@gmail.com', 'Joe User');     
-                            $mail->addReplyTo('uscew@gmail.com', 'Information'); 
-
-                            //Content
-                            $mail->isHTML(true);                                  //Set email format to HTML
-                            $mail->Subject = 'Here is the subject';
-                            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                            $mail->send();
-                            echo 'Message has been sent';
-                            header("Location:index.php");
-                        } catch (Exception $e) {
-                            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                        }
 
                 } else {
                 echo "Error: " . $sql . "<br>" . $con->error;
