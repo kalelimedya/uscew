163f8b26e101c5643869a5f33a739c43dc897d82
YigithanGumus <yigithangumuss@gmail.com>
2023-02-16 04:01:15 +0300

    a
    a

--- header.php
+++ header.php
@@ -65,7 +65,7 @@ This script is wrtten by
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;500&display=swap" rel="stylesheet">  
 
   <script>
     grecaptcha.enterprise.ready(function () {
